ff8147ccfea2ac301e31abdb60c06f0a5dfeff54
Tatiana Lepesha <tatiana.lepesha@workhuman.com>
2021-12-21 10:14:53 +0300

    1st hometask

---
+++ lesson1/task1.py
@@ -0,0 +1,34 @@
+none_1 = None
+num_1 = 8
+num_2 = 8.11
+str_1 = 'stroka'
+bool_1 = True
+bool_2 = False
+bool_3 = 0
+bool_4 = 1
+list_1 = [1, 2, 3, 4, 2.2, 'hey']
+tuple_1 = (0, 1, 2, 3.3, 'hello')
+dict_1 = {
+    'key_1': 'value_1',
+    'key_2': 'value_2',
+    'key_3': None}
+set_1 = set('long long long long string')
+
+print(none_1)
+print(num_1)
+print(num_2)
+print(str_1)
+print(bool_1, bool_2, bool_3, bool_4)
+print(list_1)
+print(tuple_1)
+print(dict_1)
+print(set_1)
+
+user_num_1 = int(input('Введите целое число: '))
+print(user_num_1)
+user_num_2 = float(input('Введите число с плавающей точкой: '))
+print(user_num_2)
+user_str_1 = input('Введите один символ: ')
+print(user_str_1)
+user_str_2 = input('Введите строку: ')
+print(user_str_2)
